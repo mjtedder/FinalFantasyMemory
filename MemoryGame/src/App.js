7ce71e8077caa3a509b2631012d5f5993f755371
mjtedder <mjtedder@gmail.com>
2018-07-23 17:28:14 -0400

    added characters JSON and coded basic app.js

--- MemoryGame/src/App.js
+++ MemoryGame/src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
-import HelloReact from "./components/HelloReact";
+import CharacterCard from "./components/CharacterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import characters from './characters.json';
 
-const App = () => <HelloReact />;
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
+
+  removeCharacter = id => {
+    // Filter this.state.character for characters with an id not equal to the id being removed
+    const characters = this.state.characters.filter(character => character.id !== id);
+    // Set this.state.characters equal to the new characters array
+    this.setState({ characters });
+  };
+
+  // Map over this.state.characters and render a CharacterCard component for each character object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Characters List</Title>
+        {this.state.characters.map(character => (
+          <CharacterCard
+            removeCharacter={this.removeCharacter}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+            occupation={character.occupation}
+            location={friend.location}
+            />
+        ))}
+    );
+  }
+}
 
 export default App;
